aa20b094006ad7410cb2850197b8e139d940ff58
정진규 <monibu1548@gmail.com>
2024-06-13 23:09:04 +0900

    Firebase 초기화

--- lib/swm_flutter_package.dart
+++ lib/swm_flutter_package.dart
@@ -1,14 +1,18 @@
 library swm_flutter_package;
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class FPKit {
   static Supabase? _supabase;
+  static FirebaseApp? _firebaseApp;
 
   static Future initialize({required String url, required String anonKey}) async {
     _supabase = await Supabase.initialize(
       url: url,
       anonKey: anonKey,
     );
+
+    _firebaseApp = await Firebase.initializeApp();
   }
 }
